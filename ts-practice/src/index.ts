617ae801a3c0e2a4141ec6e76f2c4c27fa1c2999
Jon Perada <6591295+jonp200@users.noreply.github.com>
2026-09-21 16:09:35 +0800

    feat: demo `const` and `let` usage

--- ts-practice/src/index.ts
+++ ts-practice/src/index.ts
@@ -1,5 +1,15 @@
+const noname = "Jon";
+let callCount = 0;
+
 function greet(name: string): string {
+    callCount++;
+
+    if (name === "") {
+        name = noname;
+    }
+
     return `Hello, ${name}!`;
 }
 
 console.log(greet("Jon"));
+console.log(`Call count: ${callCount}`);
